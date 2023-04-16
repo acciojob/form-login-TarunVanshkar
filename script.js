bf19647280de43116b28a5018db80081318a99af
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-04-16 17:08:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	const input = document.getElementById("form1");
+    const firstName = input.children[0].value;
+    const lastName = input.children[2].value;
+    let ans=firstName+" "+lastName;
+    
+    alert(ans);
 
 }
